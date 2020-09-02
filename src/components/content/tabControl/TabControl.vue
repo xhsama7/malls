6d68000aee801e0ed63cb2dc42ce80abd95ad81d
xhsama7 <xhhthy13@gmail>
2020-09-02 21:14:34 +0800

    '更新'

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(m, index) in titles"
+         class="tab-control-m"
+         :class="{active: index === currentIndex}" @click="mClick(index)">
+      <span>{{m}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props:{
+      titles:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    data(){
+      return{
+        currentIndex: 0
+      }
+    },
+    methods:{
+      mClick(index){
+        this.currentIndex = index
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.tab-control{
+  display: flex;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+  .tab-control-m{
+    flex: 1;
+  }
+  .active{
+    color:var(--color-high-text)
+  }
+</style>
--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -3,6 +3,9 @@
     <nav-bar class="shop-nav"><div slot="center">购物街</div></nav-bar>
     <shop-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','潮流']"></tab-control>
+
   </div>
 </template>
 
@@ -10,8 +13,11 @@
   import NavBar from "components/common/navbar/NavBar";
   import ShopSwiper from "./childComps/ShopSwiper";
   import RecommendView from "./childComps/RecommendView";
-  import {getShopMultidata} from "network/shop";
+  import FeatureView from "./childComps/FeatureView";
+  import TabControl from "../../components/content/tabControl/TabControl";
 
+  import {getShopMultidata} from "network/shop";
+  import {getHomeGoods} from "network/shop";
 
 
   export default {
@@ -19,13 +25,20 @@
     components:{
       NavBar,
       ShopSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     data(){
       return{
         //创建变量保存数据
         banners:[],
-        recommends:[]
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'news':{page:0,list:[]},
+          'sell':{page:0,list:[]}
+        }
       }
     },
     created() {
@@ -35,13 +48,28 @@
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list
       })
+      getHomeGoods('pop',1).then(res => {
+        console.log(res)
+      })
     }
   }
 </script>
 
 <style scoped>
+  #shop{
+    padding-top: 44px;
+  }
   .shop-nav{
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top:44px;
   }
 </style>
